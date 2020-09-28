bcc204bad62ac4f7aec9426a39c0ae4c8baf8f81
snehac95 <56540390+snehac95@users.noreply.github.com>
2020-09-28 22:07:44 +0530

    Add files via upload

---
+++ springboot1/src/main/java/com/boot/springboot1/springcontroller/BootController.java
@@ -0,0 +1,37 @@
+package com.boot.springboot1.springcontroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class BootController {
+	
+		public BootController() {
+			System.out.println("Created " + this.getClass().getSimpleName());
+		}
+
+		@RequestMapping("hereme")
+		public String onClick() {
+			System.out.println("Invoked hereme");
+			return "signin.jsp";
+		}
+
+		@RequestMapping("signin")
+		public String WelcomeMessage(String from, String message, String to, Model model) {
+			try {
+				System.out.println("Invoked welcome page and redirecting to signup.jsp");
+
+				model.addAttribute("from", from);
+				model.addAttribute("message", message);
+				model.addAttribute("to", to);
+			} catch (Exception e) {
+				System.err.println("Exception in welcome controller " + e);
+			}
+			return "signup.jsp";
+		}
+		
+	}
+
+
+
+
